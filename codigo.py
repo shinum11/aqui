2b9e3df6d90c28b51f59252af155be2f0e95fff8
sidnei <sidneigoes23@gmail.com>
2023-12-06 17:03:14 -0300

    Apagando comentarios

---
+++ codigo.py
@@ -0,0 +1,10 @@
+class Itens():
+    def __init__(self, nome, peso, valor):
+        self.nome = nome
+        self.peso = peso
+        self.valor = valor
+
+
+teste = Itens("claudio",10,40)
+    
+print(teste.peso)
